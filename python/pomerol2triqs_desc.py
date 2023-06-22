1486c9655f061443632a865ad48eb6cf488c3d92
Nils Wentzell <nwentzell@flatironinstitute.org>
2023-06-22 12:23:56 -0400

    Include mesh converter in desc file

--- python/pomerol2triqs_desc.py
+++ python/pomerol2triqs_desc.py
@@ -22,6 +22,7 @@ module.add_preamble("""
 #include <cpp2py/converters/variant.hpp>
 #include <cpp2py/converters/vector.hpp>
 #include <triqs/cpp2py_converters/gf.hpp>
+#include <triqs/cpp2py_converters/mesh.hpp>
 #include <triqs/cpp2py_converters/operators_real_complex.hpp>
 #include <triqs/cpp2py_converters/real_or_complex.hpp>
 
